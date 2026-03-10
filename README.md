23d4be606fb6d6ae30a794e945e74d28b2394b39
Teun217 <teun.dames2003@gmail.com>
2026-03-10 15:39:30 +0100

    add files

---
+++ README.md
@@ -0,0 +1,41 @@
+*Progression log
+
+** 16/02/2026
+
+Ik heb de eerste pagina van de HTML gestructured en ben nu bezig met zorgen dat de vragen pas te zien zijn als ze van nut zijn.
+
+*** Wat heb ik gedaan vandaag?
+De HTML van pagina 1 er in gezet.
+Geleerd over fieldsets en 
+
+
+** 17/02/2026
+
+*** Wat heb ik vandaag gedaan?
+Laatste dingetjes HTML bijgewerkt.
+
+Gewerkt aan de progressive disclosure.
+- HTML classes en divs toegevoegd.
+- CSS code
+
+General styling:
+- Variable kleuren opgezet
+- Variable fonts opgezet
+- General styling
+
+*** Wat heb ik geleerd?
+Forms zitten er een stuk beter in.
+Ik heb het idee dat de basiskennis vrijwel volledig terug is dus het html schrijven gaat een stuk makkelijker.
+
+Ik heb gelet op het overzichtelijk houden van mijn code en ik ben blij om te zien dat dat redelijk goed lukt.
+
+*** Wat ga ik volgende keer doen?
+De andere flow maken.
+
+
+*Bronnen
+<!-- Designkit NS -->
+https://tractie.ns.nl/2e23992f3/p/669cde-colors
+
+<!-- Formulier NS -->
+https://restitutie-formulier.ns.nl/ns/klantenservice-geld-terug-terugbetaling-aanvragen
---
+++ script.js
@@ -0,0 +1,99 @@
+// MARK: CONSTANTS & VARIABLES
+
+// add and remove person
+const addPersonButton = document.querySelector(".addPerson");
+const everyRemovePersonButton = document.querySelectorAll(".removePerson");
+
+//validation
+const everyRequiredQuestion = document.querySelectorAll("[required]")
+const everyProgressiveQuestionActiveButton = document.querySelectorAll(".progressiveQuestionGreyed label:first-of-type [type='radio'], .progressiveQuestionHide label:first-of-type [type='radio']")
+const everyProgressiveQuestionInactiveButton = document.querySelectorAll(".progressiveQuestionGreyed label:nth-of-type(2) [type='radio'], .progressiveQuestionHide label:nth-of-type(2) [type='radio']")
+console.log(everyProgressiveQuestionActiveButton)
+console.log(everyProgressiveQuestionInactiveButton)
+
+// MARK: ADD AND REMOVE PERSON
+
+addPersonButton.addEventListener("click", injectHTMLOnClick);
+
+everyRemovePersonButton.forEach(function (removePersonButton) {
+    removePersonButton.addEventListener("click", removePersonFunction);
+});
+
+function injectHTMLOnClick() {
+    addPersonButton.insertAdjacentHTML(
+        "beforebegin",
+        `
+        <fieldset class="verkrijgers">
+            <legend>Verkrijger</legend>
+            <button type="button" class="removePerson"></button>
+            <fieldset>
+                <legend>Persoongegevens</legend>
+                <div class="name">
+                    <label>
+                        Voorletter(s)
+                        <input type="text" name="voorletters">
+                    </label>
+                    <label>
+                        Tussenvoegsel
+                        <input type="text" name="tussenvoegsel">
+                    </label>
+                    <label>
+                        Achternaam
+                        <input type="text" name="achternaam">
+                    </label>
+                </div>
+                <label>
+                    Bsn/RSIN
+                    <input type="text" name="bsn_rsin">
+                </label>
+            </fieldset>
+
+            <fieldset>
+                <legend>Krijgt deze verkrijger waarvoor u geen aangifte doet het hele vermogen?</legend>
+                <label><input type="radio" name="geheleVermogen_${Date.now()}"> Ja</label>
+                <label><input type="radio" name="geheleVermogen_${Date.now() + 1}"> Nee</label>
+            </fieldset>
+
+            <fieldset>
+                <legend>Doet deze verkrijger een beroep op diens legitieme portie (wettelijke erfdeel)?</legend>
+                <label><input type="radio" name="legitiemePortie_${Date.now()}"> Ja</label>
+                <label><input type="radio" name="legitiemePortie_${Date.now() + 1}"> Nee</label>
+            </fieldset>
+        </fieldset>
+        `
+    );
+
+    const allVerkrijgers = document.querySelectorAll(".verkrijgers");
+    const newestVerkrijger = allVerkrijgers[allVerkrijgers.length - 1];
+    newestVerkrijger
+        .querySelector(".removePerson")
+        .addEventListener("click", removePersonFunction);
+}
+
+function removePersonFunction() {
+    this.closest(".verkrijgers").remove();
+}
+
+// MARK: DISABLE POINTER EVENTS NONE QUESTIONS AND RESET RADIOBUTTONS
+everyProgressiveQuestionActiveButton.forEach(function(oneProgressiveQuestionActiveButton){
+    oneProgressiveQuestionActiveButton.addEventListener("click", disableRequired)
+})
+
+function disableRequired(event){
+    const parentFieldset = event.target.closest(".progressiveQuestionGreyed, .progressiveQuestionHide");
+    const targetSibling = parentFieldset.nextElementSibling;
+
+    targetSibling.querySelectorAll("input[type='radio'], input[type='file']").forEach(r => r.required = true)
+}
+
+everyProgressiveQuestionInactiveButton.forEach(function(oneProgressiveQuestionInactiveButton){
+    oneProgressiveQuestionInactiveButton.addEventListener("click", resetTargetButtons)
+})
+
+function resetTargetButtons(event) {
+    const parentFieldset = event.target.closest(".progressiveQuestionGreyed, .progressiveQuestionHide");
+    const targetSibling = parentFieldset.nextElementSibling;
+
+    targetSibling.querySelectorAll("input[type='radio']").forEach(r => r.checked = false)
+    targetSibling.querySelectorAll("input[type='radio'], input[type='file']").forEach(r => r.required = false)
+}
